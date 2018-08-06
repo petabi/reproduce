b9cc2f5fc15d7be3f4e97e3f9825ab77be501faa
Kim Hyun Woo <royalwoo@petabi.com>
2018-08-06 01:04:44 +0000

    merge changes

--- src/demo.cpp
+++ src/demo.cpp
@@ -14,13 +14,8 @@ const char* topic = "pcap";
 void help(void)
 {
   cout << "[USAGE] " << program_name << " OPTIONS" << '\n';
-<<<<<<< HEAD
-  cout << "  -d: debug" << '\n';
-  cout << "  -e: evaluation" << '\n';
-=======
   cout << "  -d: debug mode (print debug messages)" << '\n';
   cout << "  -e: evaluation mode (report statistics)" << '\n';
->>>>>>> 091ae0eb704645308ce8442e0f716edfc4ab5c12
   cout << "  -f: tcpdump filter" << '\n';
   cout << "  -h: help" << '\n';
   cout << "  -i: input pcapfile or nic (mandatory)" << '\n';
@@ -82,7 +77,6 @@ int main(int argc, char** argv)
     while (!end) {
       message = pcap.get_next_stream();
       if (!message.empty()) {
-<<<<<<< HEAD
 		if (!opt.kafka)
 			rp.produce(message);
 		opt.byte += message.length();
@@ -90,11 +84,6 @@ int main(int argc, char** argv)
         opt.set_now();
 		if (opt.debug)
 			opt.mprint("%s", message.c_str());
-=======
-        rp.produce(message);
-        opt.set_now();
-        opt.mprint("%s", message.c_str());
->>>>>>> 091ae0eb704645308ce8442e0f716edfc4ab5c12
       } else {
         end = true;
       }
