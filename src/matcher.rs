3df8b5a80257234ee5a0cb306608558f3b25d0e3
Min Shao <mshao@petabi.com>
2021-03-12 15:37:14 -0800

    use hyperscan for x86_64 only

--- src/matcher.rs
+++ src/matcher.rs
@@ -1,13 +1,21 @@
 use anyhow::Result;
+#[cfg(target_arch = "x86_64")]
 use hyperscan::prelude::*;
+#[cfg(not(target_arch = "x86_64"))]
+use regex::bytes::RegexSet;
 use std::fs;
 
 pub struct Matcher {
+    #[cfg(target_arch = "x86_64")]
     db: BlockDatabase,
+    #[cfg(not(target_arch = "x86_64"))]
+    db: RegexSet,
+    #[cfg(target_arch = "x86_64")]
     scratch: Scratch,
 }
 
 impl Matcher {
+    #[cfg(target_arch = "x86_64")]
     pub fn with_file(filename: &str) -> Result<Self> {
         let patterns: Patterns = fs::read_to_string(filename)?.parse()?;
         let db: BlockDatabase = patterns.build()?;
@@ -15,6 +23,15 @@ impl Matcher {
         Ok(Matcher { db, scratch })
     }
 
+    #[cfg(not(target_arch = "x86_64"))]
+    pub fn with_file(filename: &str) -> Result<Self> {
+        let fs = fs::read_to_string(filename)?;
+        let rules = trim_to_rules(&fs);
+        let db: RegexSet = RegexSet::new(rules)?;
+        Ok(Matcher { db })
+    }
+
+    #[cfg(target_arch = "x86_64")]
     pub fn scan(&mut self, data: &[u8]) -> Result<bool> {
         let mut is_matched = false;
         self.db.scan(data, &self.scratch, |_, _, _, _| {
@@ -23,4 +40,31 @@ impl Matcher {
         })?;
         Ok(is_matched)
     }
+
+    #[cfg(not(target_arch = "x86_64"))]
+    pub fn scan(&mut self, data: &[u8]) -> Result<bool> {
+        Ok(self.db.is_match(data))
+    }
+}
+
+#[cfg(not(target_arch = "x86_64"))]
+fn trim_to_rules(s: &str) -> Vec<&str> {
+    s.lines()
+        .flat_map(|line| {
+            let line = line.trim();
+
+            if line.is_empty() || line.starts_with('#') {
+                None
+            } else {
+                let expr = match line.find(":/") {
+                    Some(off) => &line[off + 1..],
+                    None => line,
+                };
+                Some(match (expr.starts_with('/'), expr.rfind('/')) {
+                    (true, Some(end)) if end > 0 => &expr[1..end],
+                    _ => expr,
+                })
+            }
+        })
+        .collect::<Vec<_>>()
 }
